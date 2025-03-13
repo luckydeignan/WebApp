f433980fc5aebe25bb7311f21e0a70faba95225a
Lucky Deignan <ldeignan@mit.edu>
2025-03-13 00:32:53 -0400

    updated gitignore

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
+
 const app = express();
 
 // Configure CORS with specific options
@@ -11,10 +13,48 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/api/data", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+const dummy = {}
+const requestBody = {
+  "audioConfig": {
+    "audioEncoding": "LINEAR16",
+    "effectsProfileId": [
+      "small-bluetooth-speaker-class-device"
+    ],
+    "pitch": 0,
+    "speakingRate": 1
+  },
+  "input": {
+    "text": "Movies, oh my gosh, I just just absolutely love them. They're like time machines taking you to different worlds and landscapes, and um, and I just can't get enough of it."
+  },
+  "voice": {
+    "languageCode": "en-US",
+    "name": "en-US-Chirp-HD-F"
+  }
+}
+
+app.post("/api/voice", (req, res) => {
+  console.log('we are here')
+  fetch("https://texttospeech.googleapis.com/v1beta1/text:synthesize", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.content),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .then(data => res.json(data))
+  .catch(error => console.error(error));
+})
+
 
 const port = 3000;
 app.listen(port, () => {
